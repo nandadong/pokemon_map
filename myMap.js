9c5526f0fd60f85a3874871fc0123d4ab5af2bdd
Dong <raitodong@gmail.com>
2016-10-19 12:54:54 -0400

    Create myMap.js

---
+++ myMap.js
@@ -0,0 +1,6 @@
+var map;
+function loadMapScenario() {
+    map = new Microsoft.Maps.Map(document.getElementById('myMap'), {
+        credentials: 'AsQJ3D5rGa6UH6OYtvSRw0ShEB1X0cjSY6Ax61cmOdHWddTSTs6h3lQcLZirOCxi'
+    });
+}
